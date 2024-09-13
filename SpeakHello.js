a854d9eb8cd1926de631dbf42fab5fa3bbe20243
omarmiaoui <omarmas060@gmail.com>
2024-09-13 12:11:46 +0100

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,7 @@
+var helloSpeaker   
+ = {};
+ var speakWord = "Hello";
+ helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+  window.helloSpeaker = helloSpeaker;  
